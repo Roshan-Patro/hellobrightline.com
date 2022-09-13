3ad929f1cc35b9d42ab68d6be589787a2204c30c
T Roshan Kumar Patro <patro.roshan1997@gmail.com>
2022-09-14 00:18:04 +0530

    progress added

--- scripts/index.js
+++ scripts/index.js
@@ -26,6 +26,93 @@ navbar_container.innerHTML = navbar();
      }
  }
 
+ /* ------------------------------------------------------------------------------------------------------
+                                           WORKING NAVBAR DROPDOWNS
+    ------------------------------------------------------------------------------------------------------- */
+    let howWorks = document.querySelector("#howItWorks");
+    let emploers = document.querySelector("#employers");
+    let forFamilies = document.querySelector("#forFamilies");
+    let careers = document.querySelector("#careers");
+    
+    
+    let dropDownWork = document.querySelector("#workdrop");
+    let dropDownEmp = document.querySelector("#emp");
+    let dropDownFam = document.querySelector("#fam");
+    let dropDownCarr = document.querySelector("#carr");
+    
+    let countWorks = 0;
+    let countEmp = 0; 
+    let countFam = 0; 
+    let countCarr = 0;
+
+    function showHowItWorks(){
+       
+            dropDownEmp.style.visibility = "hidden";
+            dropDownFam.style.visibility = "hidden";
+            dropDownCarr.style.visibility = "hidden";
+            if(countWorks%2===0)
+            {
+                dropDownWork.style.visibility = "visible";
+            }
+            else{
+                dropDownWork.style.visibility = "hidden";
+            }
+            countWorks++;
+        // }
+    }
+    
+    
+    // Emploers
+    
+    
+    function showEmployers()
+    {
+            dropDownWork.style.visibility = "hidden";
+            dropDownFam.style.visibility = "hidden";
+            dropDownCarr.style.visibility = "hidden";
+            dropDownEmp.style.visibility = "visible";
+            if(countEmp%2===0)
+        {
+            dropDownEmp.style.visibility = "visible";
+        }
+        else{
+            dropDownEmp.style.visibility = "hidden";
+        }
+        countEmp++;
+    }
+    
+    // // For families
+    
+    
+    function showforFamilies()
+    {
+            if(countFam%2===0)
+        {
+            dropDownFam.style.visibility = "visible";
+        }
+        else{
+            dropDownFam.style.visibility = "hidden";
+        }
+        countFam++;
+    }    
+    
+    function showCareers()
+    {
+            if(countCarr%2===0)
+        {
+            dropDownCarr.style.visibility = "visible";
+        }
+        else{
+            dropDownCarr.style.visibility = "hidden";
+        }
+        countCarr++;
+    }
+    
+    howWorks.addEventListener("click", showHowItWorks);
+    emploers.addEventListener("click", showEmployers);
+    forFamilies.addEventListener("click", showforFamilies);
+    careers.addEventListener("click", showCareers);
+
 /* ------------------------------------------------------------------------------------------------------
                                 RUNNING TEXTS IN THE PORTION JUST BELOW THE NAVBAR
     ------------------------------------------------------------------------------------------------------- */
